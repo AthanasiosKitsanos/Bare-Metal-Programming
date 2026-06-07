192b1923af93935c8a376576cc5247974fa41d69
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2026-06-07 23:05:13 +0300

    Finished input class and implementation

--- utilities/io/input/terminal_input.cpp
+++ utilities/io/input/terminal_input.cpp
@@ -1,13 +1,52 @@
 #include "terminal_input.h"
-#include "keyboard/keyboard.h"
-
 
 namespace
 {
-    
+    [[gnu::always_inline]]
+    inline void move_data_right(char* end, const char* begin) noexcept
+    {
+        for(; end > begin; --end) *end = *(end - 1);
+    }
+
+    [[gnu::always_inline]]
+    inline void move_data_left(char* begin, const char* end) noexcept
+    {
+        for(; begin < end; ++begin) *begin = *(begin + 1);
+    }
 }
 
 namespace terminal
 {
-    
+    input::input(): command_buffer{}, cursor{command_buffer}, data_end{command_buffer}, buffer_end{command_buffer + command_capacity}
+    {}
+
+    bool input::add_character(const char c) noexcept
+    {
+        if(buffer_full()) return false;
+        if(*cursor != '\0')
+        {
+            move_data_right(data_end, cursor);
+        }
+        *cursor = c;
+        ++cursor;
+        ++data_end;
+        return true;
+    }
+
+    void input::delete_character() noexcept
+    {
+        if(buffer_begin()) return;
+        --cursor;
+        move_data_left(cursor, data_end);
+        --data_end;
+        *data_end = '\0';
+    }
+
+    void input::reset_buffer() noexcept
+    {
+        cursor = command_buffer;
+        data_end = command_buffer;
+        for(; cursor < buffer_end; ++cursor) *cursor = '\0';
+        cursor = command_buffer;
+    }
 }
--- utilities/io/input/terminal_input.h
+++ utilities/io/input/terminal_input.h
@@ -8,8 +8,40 @@ namespace terminal
     {
         static constexpr uint8_t command_capacity{128};
         char command_buffer[command_capacity + 1];
-        char* writable_data;
+        char* cursor;
         char* data_end;
-        bool command_ready;
+        const char* buffer_end;
+
+        [[gnu::always_inline]]
+        inline bool buffer_full() const noexcept { return data_end == buffer_end - 1; }
+
+        [[gnu::always_inline]]
+        inline bool buffer_begin() const noexcept { return cursor == command_buffer; }
+
+        public:
+            explicit input() noexcept;
+
+            bool add_character(const char c) noexcept;
+
+            void delete_character() noexcept;
+
+            [[gnu::always_inline]]
+            inline const char* read_buffer() const noexcept { return command_buffer; }
+
+            [[gnu::always_inline]]
+            inline void move_cursor_left() noexcept
+            { 
+                if(cursor == command_buffer) return;
+                --cursor;
+            }
+
+            [[gnu::always_inline]]
+            inline void move_cursor_right() noexcept
+            { 
+                if(cursor == data_end) return;
+                ++cursor;
+            }
+
+            void reset_buffer() noexcept;
     };
 }
